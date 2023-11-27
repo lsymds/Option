c9c3faf21320660c21a9582591d25798d265162a
Liam Symonds <liam@liamsymonds.com>
2023-11-27 21:47:42 +0000

    feat: Add Option<T>.Map and Option<T>.OrElse functions

--- src/Option/Option`1.cs
+++ src/Option/Option`1.cs
@@ -45,6 +45,33 @@ public class Option<T>
     }
 
     /// <summary>
+    /// If <see cref="IsSome"/> is true, maps the value of <see cref="Some"/> to a value of type <see cref="TMap"/>
+    /// using the provided mapping function. Otherwise, returns an <see cref="Option{T}"/> in a 'None' state.
+    /// </summary>
+    /// <param name="someMapper">
+    /// The mapper to apply to the current <see cref="Some"/> value if <see cref="IsSome"/> is true.
+    /// </param>
+    /// <typeparam name="TMap">The type to map to.</typeparam>
+    public Option<TMap> Map<TMap>(Func<T, TMap> someMapper)
+    {
+        return IsSome
+            ? new Option<TMap>(someMapper(Some))
+            : new Option<TMap>();
+    }
+
+    /// <summary>
+    /// Returns the current <see cref="Option"/> if <see cref="IsSome"/> is true, else returns a new <see cref="Option"/>
+    /// in a 'Some' state with a value of <see cref="@else"/>.
+    /// </summary>
+    /// <param name="else">The alternative value to return if <see cref="IsNone"/> is true.</param>
+    public Option<T> OrElse(T @else)
+    {
+        return IsSome
+            ? this
+            : new Option<T>(@else);
+    }
+
+    /// <summary>
     /// Creates an <see cref="Option{T}"/> in a 'None' state (without a value).
     /// </summary>
     public static Option<T> None()
---
+++ test/Option.Tests/OptionOrElseTests.cs
@@ -0,0 +1,34 @@
+﻿namespace LSymds.Option;
+
+public class OptionOrElseTests
+{
+    [Fact]
+    public void ReturnsTheExistingOption_IfTheOptionIsSome()
+    {
+        // Arrange.
+        var option = Option.Some("Hello, world.");
+
+        // Act.
+        var result = option.OrElse("foo");
+
+        // Assert.
+        result.IsSome.ShouldBeTrue();
+        result.IsNone.ShouldBeFalse();
+        result.Some.ShouldBe("Hello, world.");
+    }
+
+    [Fact]
+    public void ReturnsANewOptionWithTheElseValue_IfTheOptionIsNone()
+    {
+        // Arrange.
+        var option = Option.None<string>();
+        
+        // Act.
+        var result = option.OrElse("foo");
+        
+        // Assert.
+        result.IsSome.ShouldBeTrue();
+        result.IsNone.ShouldBeFalse();
+        result.Some.ShouldBe("foo");
+    }
+}
